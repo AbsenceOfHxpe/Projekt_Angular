98e0f1441dde7bee2e8d384163dcd2f10af31597
Michał Ziniewicz <michal.ziniewicz22@gmail.com>
2023-12-17 22:04:04 +0100

    wypozyczenie fix

--- src/app/zwrot/zwrot.component.ts
+++ src/app/zwrot/zwrot.component.ts
@@ -8,38 +8,37 @@ import { Rower } from '../rower';
 @Component({
   selector: 'app-zwrot',
   templateUrl: './zwrot.component.html',
-  styleUrl: './zwrot.component.css'
+  styleUrls: ['./zwrot.component.css']
 })
-export class ZwrotComponent implements OnInit{
-  private route = inject(ActivatedRoute);
-  borrowId!:number;
-  bike:Rower;
-  borrow:Wypozyczenie;
-  hoursBorrowed:number;
-  hoursBorrowedAsDate:number;
-  private wypozyczenieService:WypozyczenieService;
-  private rowerService:RowersService;
-  private router: Router;
+export class ZwrotComponent implements OnInit {
+  borrowId!: number;
+  bike: Rower;
+  borrow: Wypozyczenie;
+  hoursBorrowed: number;
+  hoursBorrowedAsDate: number;
 
-  constructor(private _rowersService:RowersService, private _wypozyczenieService:WypozyczenieService, private _router: Router){
-    this.rowerService = _rowersService;
-    this.wypozyczenieService = _wypozyczenieService;
-    this.borrow = this.wypozyczenieService.getWynajetyRower(this.borrowId);
-    this.bike = this.rowerService.getRower(this.borrow.idRoweru);
-    this.router = _router;
-    this.hoursBorrowedAsDate = new Date().getTime() - new Date(this.borrow.dataWynajmu).getTime();
-    this.hoursBorrowed = (new Date().getTime() - new Date(this.borrow.dataWynajmu).getTime())/ (3600 * 1000);
-     
-  }
+  constructor(
+    private _rowersService: RowersService,
+    private _wypozyczenieService: WypozyczenieService,
+    private _router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     const borrowId = Number(this.route.snapshot.paramMap.get('id'));
     this.borrowId = borrowId;
-    //this.borrow = this.wypozyczenieService.getWynajetyRower(this.borrowId);
+    this.borrow = this._wypozyczenieService.getWynajetyRower(this.borrowId);
+    this.bike = this._rowersService.getRower(this.borrow.idRoweru);
+    
+    this.hoursBorrowedAsDate =
+      new Date().getTime() - new Date(this.borrow.dataWynajmu).getTime();
+    this.hoursBorrowed =
+      (new Date().getTime() - new Date(this.borrow.dataWynajmu).getTime()) /
+      (3600 * 1000);
   }
 
-  deleteWynajetyRower(id:number) {
-    this.wypozyczenieService.deleteWynajetyRower(id);
-    this.router.navigate(['/wypozyczenia-component']);
+  deleteWynajetyRower(id: number) {
+    this._wypozyczenieService.deleteWynajetyRower(id);
+    this._router.navigate(['/wypozyczenia-component']);
   }
 }
