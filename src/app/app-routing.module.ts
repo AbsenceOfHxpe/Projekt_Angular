fbdebd67e6973a282facba6485f66c1f072f0b8d
Okrameister <dawid.kob02@gmail.com>
2023-12-17 14:10:23 +0100

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RowerAdminComponent } from './rower-admin/rower-admin.component';
 import { EditRowerComponent } from './edit-rower/edit-rower.component';
+import { AddRowerComponent } from './add-rower/add-rower.component';
+
 
 const routes: Routes = [
-  { path: 'rower-admin-component', component: RowerAdminComponent },
-  { path: 'edit-rower-component/:id', component: EditRowerComponent }
+  { path: 'rower-admin-component/edit-rower-component/:id', component: EditRowerComponent },
+  { path: 'rower-admin-component/add-rower-component', component: AddRowerComponent},
+  { path: 'rower-admin-component', component: RowerAdminComponent }
 ];
 
 
